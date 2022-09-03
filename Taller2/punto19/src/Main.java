393a4f534371925d5d65107b4a712f64312fc238
Camilo Florez <43497845+Xetriflo@users.noreply.github.com>
2022-09-03 10:46:46 -0500

    Add files via upload

---
+++ Taller2/punto19/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite el valor del lado: ");
+        double lado= sc.nextDouble();
+        double perimetro= lado*3;
+        double altura= Math.sqrt(3*lado)/2;
+        double area= Math.sqrt(3)/4*Math.pow(lado,2);
+
+        System.out.println("Perimetro: "+perimetro);
+        System.out.println("Altura: "+altura);
+        System.out.println("Area: "+area);
+    }
+}
